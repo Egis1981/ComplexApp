2fd79a68115b49af64627355423c1e4bf42052cd
Egis1981 <ebaranauskas@gmail.com>
2020-06-02 12:48:10 +0300

    more code added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
 const User = require('../models/User')
 
+exports.mustBeLoggedIn = function(req, res, next) {
+    if (req.session.user) {
+        next()
+    } else {
+        req.flash("errors", "You must be logged in to perform that action.")
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }
+}
+
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
-        req.session.user = {avatar: user.avatar, username: user.data.username}
+        req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
         req.session.save(function() {
             res.redirect('/')
         })
@@ -22,7 +33,7 @@ exports.logout = function(req, res) {
 exports.register = function(req, res) {
     let user = new User(req.body)
     user.register().then(() => {
-        req.session.user = {username: user.data.username, avatar: user.avatar}
+        req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
         req.session.save(function() {
             res.redirect('/')
         })
@@ -38,8 +49,24 @@ exports.register = function(req, res) {
 
 exports.home = function(req, res) {
     if (req.session.user) {
-        res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
+        res.render('home-dashboard')
     } else {
         res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
     }
+}
+
+exports.ifUserExists = function(req, res, next) {
+    User.findByUsername(req.params.username).then(function(userDocument) {
+        req.profileUser = userDocument
+        next()
+    }).catch(function() {
+        res.render("404")
+    })
+}
+
+exports.profilePostsScreen = function(req, res) {
+    res.render('profile', {
+        profileUsername: req.profileUser.username,
+        profileAvatar: req.profileUser.avatar
+    })
 }
